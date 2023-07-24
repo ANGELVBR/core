32492da7e6273bfda5bb45647b640fd881287fc8
angel.villabrasero <angelvbr@ext.inditex.com>
2023-07-24 08:10:24 +0200

    feat: Add docker-compose and drop tables

---
+++ myApp/database/schema_drop.sql
@@ -0,0 +1,16 @@
+-----------------------------------------------
+-- DROP ALL TABLE IN DATABASE APP_MANAGEMENT --
+-- VERSION 1.0                               --
+-----------------------------------------------
+DROP TABLE app_management.comments;
+DROP TABLE app_management.profiles;
+DROP TABLE app_management.task_assigments;
+DROP TABLE app_management.task_steps;
+DROP TABLE app_management.tasks 
+DROP TABLE app_management.projects;
+DROP TABLE app_management.users_roles;
+DROP TABLE app_management.priority;
+DROP TABLE app_management.users;
+DROP TABLE app_management.roles;
+DROP TABLE app_management.status_project; 
+DROP TABLE app_management.status_task;
